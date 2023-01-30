6e7fc2768bfe0d918e573ce6a8eebe8bdbf79db2
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-01-30 14:37:56 +0000

    changes to html

--- cards.js
+++ cards.js
@@ -4,35 +4,41 @@ export const cards = [
     image: "images/thai-1.png",
     header: "Publications",
     link: "/publications.html",
+    imgLink: "/publications.html",
   },
   // 2
   {
     image: "images/thai-2.png",
     header: "Research",
     link: "/research.html",
+    imgLink: "/research.html",
   },
   // 3
   {
     image: "images/thai-3.png",
     header: "Events",
     link: "/events.html",
+    imgLink: "/events.html",
   },
   // 4
   {
     image: "images/thai-4.png",
     header: "Podcasts",
     link: "/podcasts.html",
+    imgLink: "/podcasts.html",
   },
   // 5
   {
     image: "images/thai-5.png",
     header: "Projects",
     link: "/projects.html",
+    imgLink: "/projects.html",
   },
   // 6
   {
     image: "images/thai-6.png",
     header: "Media",
     link: "/media.html",
+    imgLink: "/media.html",
   },
 ];
--- index.js
+++ index.js
@@ -6,12 +6,13 @@ function distributeCards(id) {
   cards.forEach(function (card) {
     id.innerHTML += `
     <div class="cardRender" id="cardRender">
+      <a href="${card.imgLink}">
         <img src="${card.image}" class="card-img" alt="image" />
         <h2 class="h2-home-style">${card.header}</h2>
         <p class="p-home-style"><a href="${card.link}">Read more</a></p>
+        </a>
     </div>
           `;
   });
 }
-
 distributeCards(cardRender);
